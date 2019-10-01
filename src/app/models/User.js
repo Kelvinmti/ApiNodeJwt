740c5b85a8ed7bcde78785c5f3a2ddea7691934c
Kelvinmti <kdevweb@gmail.com>
2019-10-01 14:07:16 -0300

    Salvando model usuario sequelize

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,21 @@
 import { Model } from 'sequelize';
-
+// add lib bcryptjs
 class User extends Model {
-  static init(sequelize) {}
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+
+  }
 }
+
+export default User;
