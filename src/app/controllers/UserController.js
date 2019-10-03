e9717be80ef736b95d6e03ac7b721f23d85130e3
Kelvin Mendes <kdevweb@gmail.com>
2019-10-02 23:37:52 -0300

    Concluindo Api

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -24,13 +24,12 @@ class UserController {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    const { id, name, email, provider } = await User.create(req.body);
+    const { id, name, email } = await User.create(req.body);
 
     return res.json({
       id,
       name,
       email,
-      provider,
     });
   }
 
@@ -57,7 +56,7 @@ class UserController {
 
     const user = await User.findByPk(req.userId);
 
-    if (email !== user.email) {
+    if (email && email !== user.email) {
       const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
@@ -69,13 +68,12 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match.' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    const { id, name } = await user.update(req.body);
 
     return res.json({
       id,
       name,
       email,
-      provider,
     });
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,10 +8,10 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.post('/users', SessionController.store);
+routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.post('/users', UserController.update);
+routes.put('/users', UserController.update);
 
 export default routes;
